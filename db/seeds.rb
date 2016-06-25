9ef523694d3cdf5fd6640ea3c0c899c022c48598
Jeffrey D King <jdking33@aol.com>
2016-06-25 20:05:28 +0000

    A30 Testing find_or_create_by in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,9 @@ posts = Post.all
  )
 end
 
+Post.find_or_create_by(title: "Unique Post Title", body: "Unique Post Body")
+Comment.find_or_create_by(body: "Unique Comment Body")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
