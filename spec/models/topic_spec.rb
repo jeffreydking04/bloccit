3bfff8f929d83f87c1f24fdf943c80dd2d805e9b
Jeffrey King <jdking33@aol.com>
2016-07-04 07:47:44 -0700

    Assignment 35 Topic Validation-Merge

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Topic, type: :model do
 
   it { is_expected.to have_many(:posts) }
 
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(15) }
+
   describe "attributes" do
     it "should not be nil" do
       expect(topic).not_to be_nil
